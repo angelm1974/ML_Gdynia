1efe031fdfc0bb16b27802260f17b178f6f35d8b
Mariusz Mol <mariusz980@gmail.com>
2022-02-15 17:37:51 +0100

    obrazy

---
+++ przyklad2/sgdclasyfikator.py
@@ -0,0 +1,6 @@
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from imutils import paths
+import argparse
+im
