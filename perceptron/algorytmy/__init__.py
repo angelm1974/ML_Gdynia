c3b77dd3d3dbc4ae958e810d92daa59dbb930716
Mariusz Mol <mariusz980@gmail.com>
2022-02-15 19:43:53 +0100

    dobry and

--- perceptron/algorytmy/__init__.py
+++
@@ -1,1 +0,0 @@
-from algorytmy.nn import Perceptron
--- perceptron/perceptron_and.py
+++ perceptron/perceptron_and.py
@@ -1,1 +1,16 @@
 from algorytmy.nn import Perceptron
+import numpy as np
+
+X=np.array([[0,0],[0,1],[1,0],[1,1]])
+y=np.array([[0],[0],[0],[1]])
+
+print('[INFO] trenowanie perceptrona...')
+p=Perceptron(X.shape[1],alpha=0.1)
+p.fit(X,y,epoki=20)
+
+print('[INFO] testowanie perceptrona...')
+
+for (x,target) in zip(X,y):
+    pred=p.predict(x)
+    print('[INFO] dane={}, prawda_podstawowa={}, predykcja={}'.format(x,target[0],pred))
+    
